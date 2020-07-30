303a3a75e6af8e7279634e9cf275ff14449c86f6
John Fairclough <johnmfairclough@gmail.com>
2020-07-30 14:40:26 +0100

    firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB2Ps9m0DkABrRa6_f04zATXUrVQM7isqg",
+    authDomain: "firegram-c2ef1.firebaseapp.com",
+    databaseURL: "https://firegram-c2ef1.firebaseio.com",
+    projectId: "firegram-c2ef1",
+    storageBucket: "firegram-c2ef1.appspot.com",
+    messagingSenderId: "682852559094",
+    appId: "1:682852559094:web:5709361ac762d166bd24d7",
+    measurementId: "G-L4R0D17MMW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
