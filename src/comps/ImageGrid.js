7689c4ba52983e830fc7e0726efb8480520038b7
John Fairclough <johnmfairclough@gmail.com>
2020-08-03 16:32:15 +0100

    firestore

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import useFirestore from '../hooks/useFirestore';
 
 const ImageGrid = () => {
+    const { docs } = useFirestore('images');
     return (
         <div className="img-grid">
         images
