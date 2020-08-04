c46d7ad4a974cdb694766db975362e1539b419a3
John Fairclough <johnmfairclough@gmail.com>
2020-08-04 16:29:31 +0100

    Updates

---
+++ precache-manifest.355bc52e2ed5df47d1271c77aca7aef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3904dc5992cd7804494fa029850d9797",
+    "url": "/Firegram/index.html"
+  },
+  {
+    "revision": "617d236fddd393d4e2a6",
+    "url": "/Firegram/static/css/main.e3bd3a6e.chunk.css"
+  },
+  {
+    "revision": "8ecda27ef28776aaba6e",
+    "url": "/Firegram/static/js/2.0b2f89c6.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/Firegram/static/js/2.0b2f89c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "617d236fddd393d4e2a6",
+    "url": "/Firegram/static/js/main.09fec847.chunk.js"
+  },
+  {
+    "revision": "51ac040677bfac14acc5",
+    "url": "/Firegram/static/js/runtime-main.c7c1e1fb.js"
+  }
+]);
