aa5ab8eab6e1581ba824eed7a6de7571e9cb5374
John Fairclough <johnmfairclough@gmail.com>
2020-08-04 13:04:14 +0100

    modal

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Title from './comps/Title';
 import UploadForm from './comps/Uploadform';
 import ImageGrid from './comps/ImageGrid';
+import Modal from '/comps/Modal';
 
 
 function App() {
+  const [selectedImg, setSelectedImg] = useState(null);
+
   return (
     <div className="App">
       <Title/>
       <UploadForm />
       <ImageGrid />
+      <Modal selectedImg={selectedImg} />
     </div>
   );
 }
